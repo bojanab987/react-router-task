75e51625e5ac10132a2f4ff77a2f0093b4adb297
bojanabb <bojana.buljic@gmail.com>
2021-11-04 14:30:49 +0100

    Add index route to fix main area goes blank

--- src/index.js
+++ src/index.js
@@ -5,14 +5,36 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Books from './routes/Books';
 import Songs from './routes/Songs';
+import Book from './routes/Book';
+import Song from './routes/Song';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="books" element={<Books />}/>
-          <Route path="songs" element={<Songs/>}/>
+          <Route path="books" element={<Books />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>Select a book to see details</p>
+                </main>
+              }
+            />
+            <Route path=":bookId" element={<Book />}/>
+          </Route>
+          <Route path="songs" element={<Songs/>}>
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>Select a song to see details</p>
+                </main>
+              }
+            />
+            <Route path=":songId" element={<Song />}/>
+          </Route>
           <Route
             path="*"
             element={
